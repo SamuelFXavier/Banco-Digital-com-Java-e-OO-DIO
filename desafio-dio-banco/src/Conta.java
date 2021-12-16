5a9e33e004cba628cbb361776dad8424795f6392
SamuelFXavier <xavierfsamuel@gmail.com>
2021-12-16 11:37:23 -0300

    Criação das classes com pilares: Abstração, Encapsulamento, Herança e Poliformismo

---
+++ desafio-dio-banco/src/Conta.java
@@ -0,0 +1,42 @@
+public abstract class Conta implements IConta{
+
+    protected static final int AGENCIA_PADRAO = 1;
+    private static int SEQUENCIAL = 1;
+
+    protected int agencia;
+    protected int numero;
+    protected double saldo;
+
+    public Conta() {
+        this.agencia = Conta.AGENCIA_PADRAO;
+        this.numero = SEQUENCIAL++;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+
+    @Override
+    public void sacar(double valor) {
+
+    }
+
+    @Override
+    public void depositar(double valor) {
+
+    }
+
+    @Override
+    public void transferir(double valor, Conta contaDestino) {
+
+    }
+}
